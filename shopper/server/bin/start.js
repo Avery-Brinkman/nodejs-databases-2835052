49a978a8e3f20d60a748af58713d4acb9533e357
Avery-Brinkman <averybrinkman@gmail.com>
2021-06-04 17:33:15 -0400

    Implementing user management system

--- shopper/server/bin/start.js
+++ shopper/server/bin/start.js
@@ -7,11 +7,12 @@ const config = require('../config');
 const App = require('../app');
 
 async function connectToMongoose() {
-  return mongoose.connect(config.mongodb.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+    return mongoose.connect(config.mongodb.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    });
 }
 
 /* Logic to start the application */
@@ -20,41 +21,42 @@ const port = process.env.PORT || '3000';
 app.set('port', port);
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port  ${port}`;
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port  ${port}`;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
 
 const server = http.createServer(app);
 function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+    const addr = server.address();
+    const bind =
+        typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
 
-  console.info(`${config.applicationName} listening on ${bind}`);
+    console.info(`${config.applicationName} listening on ${bind}`);
 }
 server.on('error', onError);
 server.on('listening', onListening);
 
 connectToMongoose()
-  .then(() => {
-    console.info('Successfully connected to MongoDB.');
-    server.listen(port);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    .then(() => {
+        console.info('Successfully connected to MongoDB.');
+        server.listen(port);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
