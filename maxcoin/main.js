21ad3fd6ec6cf8beaadc9cd40365a66ecc004439
Avery-Brinkman <averybrinkman@gmail.com>
2021-06-07 17:39:13 -0400

    Using Redis

--- maxcoin/main.js
+++ maxcoin/main.js
@@ -1,12 +1,18 @@
 const MongoBackend = require('./services/backend/MongoBackend');
+const RedisBackend = require('./services/backend/RedisBackend');
 
-async function run() {
-  const mongoBackend = new MongoBackend();
-  return mongoBackend.max();
+async function runMongo() {
+    const mongoBackend = new MongoBackend();
+    return mongoBackend.max();
 }
 
-run()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => console.error(err));
+async function runRedis() {
+    const redisBackend = new RedisBackend();
+    return redisBackend.max();
+}
+
+runRedis()
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => console.error(err));
