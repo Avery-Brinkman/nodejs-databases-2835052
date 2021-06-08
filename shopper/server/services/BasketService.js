4f966f69b7ce3398fb8dae5f86ec29942c3cfd85
Avery-Brinkman <averybrinkman@gmail.com>
2021-06-08 14:45:01 -0400

    Adding basket to page

--- shopper/server/services/BasketService.js
+++ shopper/server/services/BasketService.js
@@ -1,19 +1,19 @@
 class BasketService {
-    constructor(client, userID) {
+    constructor(client, userId) {
         this.client = client;
-        this.key = `basket:${userID}`;
+        this.key = `basket:${userId}`;
     }
 
-    async add(itemID) {
-        return this.client.hincrby(this.key, itemID, 1);
+    async add(itemId) {
+        return this.client.hincrby(this.key, itemId, 1);
     }
 
     async getAll() {
         return this.client.hgetall(this.key);
     }
 
-    async remove(itemID) {
-        return this.client.hdel(this.key, itemID);
+    async remove(itemId) {
+        return this.client.hdel(this.key, itemId);
     }
 }
 
